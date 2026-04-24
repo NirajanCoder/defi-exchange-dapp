27f3536813d9259f87e2d5efe04c544296ed713a
Nirajan Baij <nirajanbaij@Nirajans-MacBook-Air.local>
2026-04-25 02:58:18 +0530

    DeFi Exchange DApp project

--- hardhat-tutorial/scripts/deploy.js
+++ hardhat-tutorial/scripts/deploy.js
@@ -1,25 +1,18 @@
 const { ethers } = require("hardhat");
-require("dotenv").config({ path: ".env" });
 const { CRYPTO_DEV_TOKEN_CONTRACT_ADDRESS } = require("../constants");
 
 async function main() {
-  const cryptoDevTokenAddress = CRYPTO_DEV_TOKEN_CONTRACT_ADDRESS;
-  /*
-  A ContractFactory in ethers.js is an abstraction used to deploy new smart contracts,
-  so exchangeContract here is a factory for instances of our Exchange contract.
-*/
   const exchangeContract = await ethers.getContractFactory("Exchange");
 
-  // here we deploy the contract
-  const deployedExhangeContract = await exchangeContract.deploy(
-    cryptoDevTokenAddress
+  const deployedExchangeContract = await exchangeContract.deploy(
+    CRYPTO_DEV_TOKEN_CONTRACT_ADDRESS
   );
 
-  // print the address of the deployed contract
-  console.log("Exchange Contract Address:", deployedExhangeContract.address);
+  await deployedExchangeContract.deployed();
+
+  console.log("Exchange Contract Address:", deployedExchangeContract.address);
 }
 
-// Call the main function and catch if there is any error
 main()
   .then(() => process.exit(0))
   .catch((error) => {
